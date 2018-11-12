43182e6063aeb4ffcc84888fe95ae8a435b1edf0
Pätsi Teemu <K9100@student.jamk.fi>
2018-11-12 03:21:17 +0200

    Updated author notes and renamed list variable 'array' to 'my_list'

--- mergesort.py
+++ mergesort.py
@@ -4,47 +4,53 @@ Description:    Algorithm for sorting integers in ascending order
 Author:         Teemu Pätsi
 Date:           11th of November 2018
 Version:        1.0.1
-Usage:          Import mergesort to python script --> mergesort(unsorted_array) 
 Python version: 3.6
+Usage:          
+    1st way     * Clone this repository
+                * Import mergesort to python script
+                * mergesort(your_unsorted_list)
+                    --> your_unsorted_list becomes sorted in ascending order
+   
+   2nd way      * Make one or more lists to main()
+                * Assign them to mergesort      // mergesort(your_unsorted_list)
+                    --> your_unsorted_list becomes sorted in ascending order
 
 Change log:
     1.0.1       Added exception handler to ensure list only contains integers or floats
 '''
 
-import sys
-import random
-from datetime import datetime
-
-def mergesort(array):
+def mergesort(my_list):
     
     # Exception handler to ensure list only contains integers or floats
     try:
-        for element in array:
+        for element in my_list:
             if (not element == float(element)):
                 raise ValueError
     except ValueError:
         print ("\nFunction mergesort only accepts lists consisting of integers or floats\nSorting was unsuccessful!\n")
     else:
-        split(array)
+        split(my_list)
 
-# Splits array to sorted subarrays
-def split(array):
-    
-    # 1 or 0 length array/substring is sorted
-    if len(array) > 1:
-        mid = len(array)//2
-        left = array[:mid]
-        right = array[mid:]
+# Splits my_list to sorted sublists
+def split(my_list):
+   
+    # print ("Split " + str(my_list))       # Delete comment to print whenever program splits list to smaller sublists
+
+    # 1 or 0 length my_list/substring is sorted
+    if len(my_list) > 1:
+        mid = len(my_list)//2
+        left = my_list[:mid]
+        right = my_list[mid:]
         
-        # Recursively splits array until subarray reaches length of 1
+        # Recursively splits my_list until sublist reaches length of 1
         split(left)
         split(right)
 
-        # Merges sorted subarrays
-        merge(array, left, right)
+        # Merges sorted sublists
+        merge(my_list, left, right)
 
-# Merges sorted subarrays
-def merge(array, left, right):
+# Merges sorted sublists
+def merge(my_list, left, right):
    
     i = j = k = 0
     
@@ -53,24 +59,24 @@ def merge(array, left, right):
 
         # Change to descending sort by changing '<' to '>'
         if left[i] < right[j]:
-            array[k] = left[i]
+            my_list[k] = left[i]
             i += 1
         else:
-            array[k] = right[j]
+            my_list[k] = right[j]
             j += 1
         k += 1
     
-    # If other subarray is empty, push rest of other side to the array due it's already sorted 
+    # If other sublist is empty, push rest of other side to the my_list due it's already sorted 
     while i < len(left):
-        array[k] = left[i]
+        my_list[k] = left[i]
         i += 1
         k += 1
     while j < len(right):
-        array[k] = right[j]
+        my_list[k] = right[j]
         j += 1
         k += 1
  
-   # print ("Merge " + str(array))      # Delete comment to print when program merges two subarrays
+    # print ("Merge " + str(my_list))      # Delete comment to print when program merges two sublists
 
 
 # Checks that program prints elapsed time right (not -320ms but 780ms)
